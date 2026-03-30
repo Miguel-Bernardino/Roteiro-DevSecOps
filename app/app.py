f726168c952ac635df5257d01af2362bf260029e
Miguel Bernardino Sousa Borges da Silva <miguel.bernardino.5891@gmail.com>
2026-03-30 23:10:31 +0000

    fix: sql injection

--- app/app.py
+++ app/app.py
@@ -32,9 +32,9 @@ def login():
         result = conn.execute(sql_Query_Not_Injection, parameters=dict(username = username_from_form
                                                                        , password = password_from_form))
         
-        sql_Query_Injection_False_Negative = text("SELECT * FROM users WHERE username = '" + username_from_form + "'"
-                                               " AND password = " + password_from_form)                                         
-        result = conn.execute(sql_Query_Injection_False_Negative)
+        #sql_Query_Injection_False_Negative = text("SELECT * FROM users WHERE username = '" + username_from_form + "'"
+        #                                       " AND password = " + password_from_form)                                         
+        #result = conn.execute(sql_Query_Injection_False_Negative)
 
         content = "<table>"
         content = content + str("<tr>")
@@ -66,9 +66,9 @@ def login():
         sql_Query_Not_Injection = text("SELECT * FROM users WHERE id=:user_id")
         result = conn.execute(sql_Query_Not_Injection, parameters=dict(user_id = id))
         
-        sql_Query_Injection_False_Negative = text("SELECT * FROM users WHERE username = '" + username_from_url + "'"
-                                                " AND password = " + password_from_url)                                         
-        result = conn.execute(sql_Query_Injection_False_Negative)
+        #sql_Query_Injection_False_Negative = text("SELECT * FROM users WHERE username = '" + username_from_url + "'"
+        #                                        " AND password = " + password_from_url)                                         
+        #result = conn.execute(sql_Query_Injection_False_Negative)
 
         # deprecated in SQLAlchemy >=2.0
         #sql_Query_Injection = "SELECT * FROM users WHERE id={}".format(id)
